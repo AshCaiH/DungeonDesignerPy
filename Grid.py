f5048dbae9df246622f9ed4b57c00184100147db
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-07-30 20:56:46 +0100

    remove empty cells from dict

--- Grid.py
+++ Grid.py
@@ -4,7 +4,7 @@ import Global, Cursor
 
 screen = None
 
-grid_dict = {}
+cell_dict : dict = {}
 
 def draw():
     global screen
@@ -18,8 +18,8 @@ def draw():
     return screen
 
 def draw_floors():
-    for key in grid_dict.keys():
-        if grid_dict[key].get("floor", 0) != 0:
+    for key in cell_dict.keys():
+        if cell_dict[key].get("floor", 0) != 0:
             pg.draw.rect(screen, (255,255,255, 150),
                         pg.Rect(*Global.cell_to_screen(Vector2(*key)), Global.CELL_SIZE, Global.CELL_SIZE))
 
@@ -29,19 +29,22 @@ def draw_props():
 
 
 def draw_walls():
-    for key in grid_dict.keys():
+    for key in cell_dict.keys():
         startPoint = tuple(Global.cell_to_screen(Vector2(*key)))
 
-        if grid_dict[key].get("nwall", 0) != 0:
+        if cell_dict[key].get("nwall", 0) != 0:
             endPoint = tuple(Global.cell_to_screen(Vector2(*key) + Vector2(1, 0)))
             pg.draw.line(screen, (255,255,255), startPoint, endPoint, 5)
             
-        if grid_dict[key].get("wwall", 0) != 0:
+        if cell_dict[key].get("wwall", 0) != 0:
             endPoint = tuple(Global.cell_to_screen(Vector2(*key) + Vector2(0, 1)))
             pg.draw.line(screen, (255,255,255), startPoint, endPoint, 5)
 
 
 def draw_grid():
+    global cell_dict
+    # Remove unused cells from dict    
+
     for i in range(Global.COLS + 1):
         x = i * Global.CELL_SIZE + Global.camera_offset.x
         pg.draw.line(screen, Global.GRID_COLOUR, 
@@ -53,13 +56,26 @@ def draw_grid():
                      (0 + Global.camera_offset.x, y), (Global.COLS * Global.CELL_SIZE + Global.camera_offset.x, y))
         
 def add_floor(cell_coord):
-    grid_dict.setdefault((cell_coord.x, cell_coord.y), {})
-    grid_dict[(cell_coord.x, cell_coord.y)]["floor"] = 1
+    cell_dict.setdefault((cell_coord.x, cell_coord.y), {})
+    cell_dict[(cell_coord.x, cell_coord.y)]["floor"] = 1
 
+def clean_cells():
+    global cell_dict
+    # Remove empty cells from dictionary.
+    def nonzero_value(item): return item[1] != 0
+
+    for key in cell_dict.keys():
+        for key2 in cell_dict[key].keys():
+            cell_dict[key] = dict(filter(nonzero_value, cell_dict[key].items()))
+
+    def contains_keys(cells : dict): return len(cells[1].keys()) > 0
+    
+    cell_dict = dict(filter(contains_keys, cell_dict.items()))
 
 # Modes: 0 = off, 1 = on, 2 = toggle
 def set_cell(cell_coord, element_state = 2):
-    grid_dict.setdefault((cell_coord.x, cell_coord.y), {})
+    global cell_dict
+    cell_dict.setdefault((cell_coord.x, cell_coord.y), {})
 
     element = None
 
@@ -69,15 +85,16 @@ def set_cell(cell_coord, element_state = 2):
         case 3: element = "wwall" 
         case 4: element = "prop" 
         case _: return
-
-
+    
     if element_state == 2:
         try:
-            if grid_dict[(cell_coord.x, cell_coord.y)][element] in [0, None]:
-                grid_dict[(cell_coord.x, cell_coord.y)][element] = 1
+            if cell_dict[(cell_coord.x, cell_coord.y)][element] in [0, None]:
+                cell_dict[(cell_coord.x, cell_coord.y)][element] = 1
             else:
-                grid_dict[(cell_coord.x, cell_coord.y)][element] = 0
+                cell_dict[(cell_coord.x, cell_coord.y)][element] = 0
         except KeyError:
-            grid_dict[(cell_coord.x, cell_coord.y)][element] = 1
+            cell_dict[(cell_coord.x, cell_coord.y)][element] = 1
     else:
-        grid_dict[(cell_coord.x, cell_coord.y)][element] = element_state
+        cell_dict[(cell_coord.x, cell_coord.y)][element] = element_state
+
+    clean_cells()
