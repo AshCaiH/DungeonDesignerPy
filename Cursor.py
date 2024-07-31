7be4018165d4555084d52dc215a95806bde1554e
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-07-31 20:30:06 +0100

    refactor wall cursor code

--- Cursor.py
+++ Cursor.py
@@ -5,7 +5,9 @@ from Global import WALL_COLLISION as hex_size
 
 cell_pos = Vector2(0,0)
 inner_pos = Vector2(0,0) # Where the mouse is within the cell
-mode = 0 # 1 = floor, 2 = nwall, 3 = wwall, 4 = prop
+mode = 0 # 1 = floor, 2 = nwall, 3 = wwall, 4 = prop, -1 = Grab
+
+override_mode = None
 
 screen = None
 
@@ -14,28 +16,29 @@ previous_mode = None
 def get_cursor_type():
     global mode
 
+    if override_mode == -1: return override_mode
+
     near_nwall = False
     near_wwall = False
     offset = Vector2(0, 0)
 
-    # Check if cursor is in grid with buffer for grid edges
+    # Check if cursor is in grid (with buffer for grid edges)
     if cell_pos.x >= Global.COLS + 1 or cell_pos.x < -1 \
     or cell_pos.y >= Global.ROWS + 1 or cell_pos.y < -1:
-            return 0
+        return 0
     
-    # Near north wall
-    if inner_pos.y > Global.CELL_SIZE - Global.CURSOR_SIZE:
-        offset.y = 1
-        near_nwall = True
-    elif inner_pos.y < Global.CURSOR_SIZE:
-        near_nwall = True
     
-    # Near west wall
-    if inner_pos.x > Global.CELL_SIZE - Global.CURSOR_SIZE:
-        offset.x = 1
-        near_wwall = True
-    elif inner_pos.x < Global.CURSOR_SIZE:
-        near_wwall = True
+    def check_near_wall(inner_pos_axis, offset_axis):
+        if inner_pos_axis > Global.CELL_SIZE - Global.CURSOR_SIZE:
+            if offset_axis == "x": offset.x = 1
+            elif offset_axis == "y": offset.y = 1
+            return True
+        elif inner_pos_axis < Global.CURSOR_SIZE: return True
+        else: return False    
+
+    near_nwall = check_near_wall(inner_pos.y, "y")
+    near_wwall = check_near_wall(inner_pos.x, "x")
+
 
     # If near both walls, prioritise last wall touched, otherwise prioritise north wall.
     # Prevents cursor "flickering" between both modes when moving it along a grid edge.
@@ -45,6 +48,7 @@ def get_cursor_type():
         if not (cell_pos.y >= Global.ROWS + 1 or cell_pos.y + offset.y < 0) \
         and not (cell_pos.x >= Global.COLS or cell_pos.x < 0):
             return 2
+    
     if near_wwall and (not near_nwall or previous_mode == 3):
         cell_pos.x += offset.x
 
@@ -59,6 +63,7 @@ def get_cursor_type():
 
     return 1
 
+
 def move(mouse_pos):
     global cell_pos, inner_pos, mode, previous_mode
     cell_pos = Global.mouse_to_cell(mouse_pos)
@@ -68,6 +73,7 @@ def move(mouse_pos):
     mode = get_cursor_type()
 
     previous_mode = mode
+
 
 def draw():
     global screen
--- Loop.py
+++ Loop.py
@@ -25,18 +25,22 @@ def tick():
             elif mods & pg.KMOD_CTRL and event.key == pg.K_l:
                 IO.load_file()
         
-        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 2:            
+        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 2:
             try:
                 Global.camera_click_pos = event.pos
+                Cursor.override_mode = -1
             except AttributeError:
                 pass
 
         elif event.type == pg.MOUSEBUTTONUP and event.button == 2:
             Global.camera_last_offset = Global.camera_offset
+            Cursor.override_mode = None
+            Cursor.move(event.pos)
         
         # Always check this
         if pg.mouse.get_pressed()[1]:
-            try: Global.camera_offset = Vector2(*event.pos) - (Global.camera_click_pos - Global.camera_last_offset)
+            try: 
+                Global.camera_offset = Vector2(*event.pos) - (Global.camera_click_pos - Global.camera_last_offset)
             except AttributeError: pass
         
     return True
